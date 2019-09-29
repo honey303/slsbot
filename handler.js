e9f30f6aa7367e831d97f10d2ac670967fffe48f
unknown <honey.narendran@balzeclan.com>
2019-09-29 22:22:16 +0530

    Translate bot

--- handler.js
+++ handler.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const axios = require('axios');
+const AWS = require('aws-sdk');
+
+const translate = new AWS.Translate();
 
 // Your first function handler
 module.exports.webhook = (event, context, callback) => {
   if (event.method === 'GET') {
-    // facebook app verification
+    // fb app verification
     if (event.query['hub.verify_token'] === 'STRONGTOKEN' && event.query['hub.challenge']) {
       return callback(null, parseInt(event.query['hub.challenge']));
     } else {
@@ -14,36 +17,58 @@ module.exports.webhook = (event, context, callback) => {
   }
 
   if (event.method === 'POST') {
-    event.body.entry.map((entry) => {
-      entry.messaging.map((messagingItem) => {
-        if (messagingItem.message && messagingItem.message.text) {
-          const accessToken = 'EAAFU2XUlmxoBAOZAwgZCX6KxshjyCghnX8i7DQ8gL5FoB4AKf6CfE883glNydrSIAheGZCpeY4DGv2LtHANUJAcC2SuHDlVb4OdHo9B2eA5BVjsjAokBGQsjFBFdsogae2IQAFrb71j6jTfXw4O4lEcu1fR4Ey84bmbSt8wMOAnDqQONAAV'; // TODO: replace this token before deployment
+      event.body.entry.map((entry) => {
+        entry.messaging.map((messagingItem) => {
+          if (messagingItem.message && messagingItem.message.text) {
+              const accessToken = 'EAAFU2XUlmxoBAApfKoOXPFxaBAJaYzeIZAbeB2caANkpZCkU0CFwTX4sZC1VjVDxf4gwNhE9QZCVAXHQVFfYnZAvKAuhpyYQX8j2N2dz7F0qoR0ZC2GhZCZBGmMIYb03V6JEvApvZBCiA2UrKwy0pqTcFjUdTYuAzxXXiPjEIvFovyhWUEI5fk1mN';
+              const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`;
 
-          const quotes = [
-            'Don\'t cry because it\'s over, smile because it happened. - Dr. Seuss',
-            'Be yourself; everyone else is already taken. - Oscar Wilde',
-            'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe. - Albert Einstein',
-            'Be who you are and say what you feel, because those who mind don\'t matter, and those who matter don\'t mind. - Bernard M. Baruch',
-            'So many books, so little time. - Frank Zappa',
-            'A room without books is like a body without a soul. - Marcus Tullius Cicero'
-          ];
+              var params = {
+              	SourceLanguageCode: 'en',
+              	TargetLanguageCode: 'fr',
+              	Text: messagingItem.message.text
+              }
 
-          const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+              translate.translateText(params, (err, data) => {
+                if (err) {
+                  console.log(err, err.stack);
+                }
+                else {
+                  const payload = {
+                      recipient: {
+                        id: messagingItem.sender.id
+                      },
+                      message: {
+                        text: data.TranslatedText
+                      }
+                  };
+                  axios.post(url, payload).then((response) => callback(null, response));
+                }
+              });
 
-          const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`;
+            // const quotes = [
+            //   'Don\'t cry because it\'s over, smile because it happened. - Dr. Seuss',
+            //   'Be yourself; everyone else is already taken. - Oscar Wilde',
+            //   'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe. - Albert Einstein',
+            //   'Be who you are and say what you feel, because those who mind don\'t matter, and those who matter don\'t mind. - Bernard M. Baruch',
+            //   'So many books, so little time. - Frank Zappa',
+            //   'A room without books is like a body without a soul. - Marcus Tullius Cicero'
+            // ];
+            //
+            // const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-          const payload = {
-            recipient: {
-              id: messagingItem.sender.id
-            },
-            message: {
-              text: randomQuote
-            }
-          };
-
-          axios.post(url, payload).then((response) => callback(null, response));
-        }
+                // const payload = {
+            //   recipient: {
+            //     id: messagingItem.sender.id
+            //   },
+            //   message: {
+            //     text: randomQuote
+            //   }
+            // };
+            //
+            // axios.post(url, payload).then((response) => callback(null, response));
+          }
+        });
       });
-    });
-  }
+    }
 };
